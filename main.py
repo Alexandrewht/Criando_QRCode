396f02dab83616686db02c917417186eb396375c
Alexandre White de mello <alexandrewhitedemello@hotmail.com>
2024-07-04 15:21:38 -0300

    modified:   main.py 	new file:   qr_code_github.png

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pyqrcode
+from pathlib import Path
+
+DIR = Path(__file__).parent
 
 qr_url = 'https://github.com/Alexandrewht'
 qr_code = pyqrcode.create(qr_url)
+save_qrcode = DIR / 'qr_code_github.png'
 
 qr_code.png(
-    file='qr_code.png',
-    scale=8,
+    file = save_qrcode,
+    scale = 10,
 )
